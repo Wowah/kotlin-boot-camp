297a6b5124b3261940aaf248f3194fdff6294fc1
Wowah <vova031297@gmail.com>
2018-10-12 05:08:17 +0300

    assignment03

--- assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
+++ assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
@@ -1,6 +1,104 @@
 package io.rybalkinsd.kotlinbootcamp
 
-class RawProfile(val rawData: String)
+class RawProfile(val rawData: String) {
+    fun toProfile(id: Long): Profile {
+        val propList = rawData.split('\n').toMutableList()
+        val result: Profile
+        var source: String? = propList.find { it.split('=')[0] == "source" }
+        if (source == null) {
+            source = "facebook"
+        } else {
+            source = source.split('=')[1]
+        }
+        propList.remove(source)
+        result = when (source) {
+            "facebook" -> FacebookProfile(id)
+            "vk" -> VkProfile(id)
+            "linkedin" -> LinkedinProfile(id)
+            else -> FacebookProfile(1)
+        }
+        propList.forEach {
+            val elem = it.split('=')
+            when (elem[0]) {
+                "firstName" -> result.firstName = elem[1].substringBefore(',')
+                "lastName" -> result.lastName = elem[1].substringBefore(',')
+                "age" -> result.age = elem[1].substringBefore(',').toIntOrNull()
+            }
+        }
+        return result
+    }
+}
+
+/**
+ * Here are Raw profiles to analyse
+ */
+val rawProfiles = listOf(
+        RawProfile("""
+            firstName=alice,
+            age=16,
+            source=facebook
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=Dent,
+            lastName=kent,
+            age=88,
+            source=linkedin
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=alla,
+            lastName=OloOlooLoasla,
+            source=vk
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=bella,
+            age=36,
+            source=vk
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=angel,
+            age=I will not tell you =),
+            source=facebook
+            """.trimIndent()
+        ),
+
+        RawProfile(
+                """
+            lastName=carol,
+            source=vk
+            age=49,
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=bob,
+            lastName=John,
+            age=47,
+            source=linkedin
+            """.trimIndent()
+        ),
+        RawProfile("""
+            lastName=kent,
+            firstName=dent,
+            age=88,
+            source=facebook
+            """.trimIndent()
+        ),
+        RawProfile("""
+            firstName=alice,
+            age=16,
+            source=vk
+            """.trimIndent()
+        ),
+        RawProfile("""
+            age=16,
+            firstName=alice,
+            source=linkedin
+            """.trimIndent()
+        )
+)
 
 enum class DataSource {
     FACEBOOK,
@@ -21,6 +119,8 @@ sealed class Profile(
  * Declare classes for all data sources
  */
 class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
+class VkProfile(id: Long) : Profile(dataSource = DataSource.VK, id = id)
+class LinkedinProfile(id: Long) : Profile(dataSource = DataSource.LINKEDIN, id = id)
 
 /**
  * Task #2
@@ -28,8 +128,20 @@ class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id =
  *
  * TODO
  */
-val avgAge: Map<DataSource, Double> = emptyMap()
+val allProfiles = rawProfiles.mapIndexed { index, x -> x.toProfile(index.toLong()) }
 
+val avgAge: Map<DataSource, Double> = DataSource.values().associate {
+    var count: Long = 0
+    val sum: Long = allProfiles.fold(0L) { acc: Long, profile ->
+        if ((profile.dataSource == it) && (profile.age != null)) {
+            count++
+            acc + profile.age!!
+        } else
+            acc
+    }
+    val avg: Double = sum.toDouble() / count
+    it to if (count > 0) avg else 0.0
+}
 /**
  * Task #3
  * Group all user ids together with all profiles of this user.
@@ -37,63 +149,8 @@ val avgAge: Map<DataSource, Double> = emptyMap()
  *
  * TODO
  */
-val groupedProfiles: Map<Long, List<Profile>> = emptyMap()
+infix fun Profile.equals(x: Profile): Boolean = (x.firstName == firstName) && (x.lastName == lastName) && (x.age == age)
 
-/**
- * Here are Raw profiles to analyse
- */
-val rawProfiles = listOf(
-    RawProfile("""
-            fisrtName=alice,
-            age=16,
-            source=facebook
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=Dent,
-            lastName=kent,
-            age=88,
-            source=linkedin
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=alla,
-            lastName=OloOlooLoasla,
-            source=vk
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=bella,
-            age=36,
-            source=vk
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=angel,
-            age=I will not tell you =),
-            source=facebook
-            """.trimIndent()
-    ),
-
-    RawProfile(
-        """
-            lastName=carol,
-            source=vk
-            age=49,
-            """.trimIndent()
-    ),
-    RawProfile("""
-            fisrtName=bob,
-            lastName=John,
-            age=47,
-            source=linkedin
-            """.trimIndent()
-    ),
-    RawProfile("""
-            lastName=kent,
-            fisrtName=dent,
-            age=88,
-            source=facebook
-            """.trimIndent()
-    )
-)
+val groupedProfiles: Map<Long, List<Profile>> = allProfiles.associate {
+    it.id to allProfiles.filter { x -> x equals it }
+}
--- assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
+++ assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
@@ -1,18 +1,37 @@
 package io.rybalkinsd.kotlinbootcamp
 
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
-
+import kotlin.math.floor
 
 class DataAnalysisTest {
 
     @Test
     fun `check avg age`() {
         assertTrue(avgAge.isNotEmpty())
+        assertEquals(avgAge.size, 3)
+    }
+
+    @Test
+    fun `check avg age value`() {
+        assertEquals(floor(avgAge.getValue(DataSource.FACEBOOK)).toInt(), 52)
+        assertEquals(floor(avgAge.getValue(DataSource.VK)).toInt(), 33)
+        assertEquals(floor(avgAge.getValue(DataSource.LINKEDIN)).toInt(), 50)
     }
 
     @Test
     fun `check grouped profiles`() {
         assertTrue(groupedProfiles.isNotEmpty())
+        assertEquals(groupedProfiles.size, allProfiles.size)
+    }
+
+    @Test
+    fun `check grouped profiles values`() {
+        assertTrue(groupedProfiles.getValue(0L).containsAll(groupedProfiles.getValue(8)))
+        assertTrue(groupedProfiles.getValue(0L).containsAll(groupedProfiles.getValue(9)))
+        for (i: Long in 1L..7L) {
+            assertEquals("Invalid list size to user with id = %d".format(i), groupedProfiles.getValue(i).size, 1)
+        }
     }
 }
